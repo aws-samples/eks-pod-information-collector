ab94aefba9333e390734d1616fbd2c1631c4f8d3
Milankumar Patel <pmh231294@gmail.com>
2023-02-02 11:08:54 -0800

    Update EKS-ClusterCrawler.sh

--- EKS-ClusterCrawler.sh
+++ EKS-ClusterCrawler.sh
@@ -1,5 +1,12 @@
 # #!/bin/bash
 
+#### NOTE #######
+#  If your present issue does not involve kubernetes resources Such as PVC, SC, PV, WorkerNode information, WebHook Configuration
+#  Feel free to comment line number 83 - 112 before executing the script.
+#  The Line number 83 - 112 of the script will collect all the information about the kubernetes resources mentioned above that are not Namespace bound within the cluster. 
+
+
+
 ROOT_OUTPUT_DIR=$PWD
 echo "======================================="
 echo "Creating a Folder at:  $ROOT_OUTPUT_DIR"
@@ -17,22 +24,36 @@ echo " ${CLUSTERNAME} Log Collected In Folder :  $OUTPUT_DIR"
 echo "======================================="
 cd $ROOT_OUTPUT_DIR
 mkdir "$OUTPUT_DIR"
-echo "Collecting information about kubernetes cluster: ${CLUSTERNAME} "
+echo "Collecting Information about kubernetes cluster: ${CLUSTERNAME}, Review file: ClusterDetails.txt "
 FILENAME="${OUTPUT_DIR}/ClusterDetails.txt"
 kubectl config current-context > "$FILENAME"
 echo "==========================" >> "$FILENAME"
 kubectl cluster-info >> "$FILENAME"
 echo "==========================" >> "$FILENAME"
 
+echo "Collecting ConfigMap details about kubernetes cluster: ${CLUSTERNAME}, Review file: ConfigMap_Info.txt "
+CONFIG="${OUTPUT_DIR}/ConfigMap_Info.txt"
+echo "[1] =========== AWS-Auth ConfigMap Details ===============" > "$CONFIG"
+kubectl describe configmap aws-auth -n kube-system >> "$CONFIG"
+echo "[1] =========== END ===============" >> "$CONFIG"
+echo "[2] =========== CoreDNS ConfigMap Details ===============" >> "$CONFIG"
+kubectl describe configmap coredns -n kube-system >> "$CONFIG"
+echo "[2] =========== END ===============" >> "$CONFIG"
+echo "[3] =========== Kube-Proxy ConfigMap Details ===============" >> "$CONFIG"
+kubectl describe configmap kube-proxy -n kube-system >> "$CONFIG"
+echo "[3] =========== END ===============" >> "$CONFIG"
 
-# Collecting All the PODs descriptions running in User desired Namespace Or by default it will collect pods log running in default namespace
+
+
+
+# Collecting All the PODs descriptions/logs running in User Desired Namespace Or by default it will collect pods log running in default namespace
 Default_Namespace=${1:-'default'}
+echo "Collecting the running POD Logs and Description from  Namespace:  ${Default_Namespace} "
 kubectl get ns --no-headers | while read -r line; do
 NAMESPACE=$(echo "$line" | awk '{print $1}')
 if [[ "$NAMESPACE" = "$Default_Namespace" ]];then
     kubectl get pods -n "$NAMESPACE" --no-headers | while read -r lines; do
             POD_NAME=$(echo "$lines" | awk '{print $1}')
-            echo "$POD_NAME" 
             FILENAME1="${OUTPUT_DIR}/${NAMESPACE}.${POD_NAME}.describe.txt"
             kubectl describe pod -n "$NAMESPACE" "$POD_NAME" > "$FILENAME1"
             for CONTAINER in $(kubectl get po -n "$NAMESPACE" "$POD_NAME" -o jsonpath="{.spec.containers[*].name}"); do
@@ -57,48 +78,49 @@ if [[ "$NAMESPACE" = "$Default_Namespace" ]];then
  fi  
 done
 
-# # Collecting All the currently deployed kubernetes resources within the cluster.
 
 
-#  -A >> "$FILENAME"
-# echo "======== END OF THE FILE ==================" >> "$FILENAME"
+# Collecting All the Worker-Node level descriptions and their performance metrics informantion.
 
-# # Collecting All the recent events logs.
-# echo "Collecting recent events log that took place within ${CLUSTERNAME}, Review file: EventsInfo.txt"
-# FILENAME1="${OUTPUT_DIR}/EventsInfo.txt"
-# kubectl get events -A --sort-by=.metadata.creationTimestamp > "$FILENAME1"
+echo "Collecting information about all the presently running worker node within ${CLUSTERNAME}, Review file: WorkerNodeInfo.txt "
+FILENAME6="${OUTPUT_DIR}/WorkerNodeInfo.txt"
+kubectl describe node -A > "$FILENAME6"
 
-# # Collecting All the Worker-Node level descriptions and their performance metrics informantion.
-# echo "Collecting information about all the presently running worker node within ${CLUSTERNAME}, Review file: WorkerNodeInfo.txt "
-# FILENAME2="${OUTPUT_DIR}/WorkerNodeInfo.txt"
-# kubectl describe node -A > "$FILENAME2"
+# Collecting All the information about persistance volume and storage class.
 
-# # Collecting All the information about persistance volume and storage class.
-# echo "Collecting information about all the persistance volume and storage class deployed ${CLUSTERNAME}, Review file: StorageInfo.txt  "
-# FILENAME3="${OUTPUT_DIR}/StorageInfo.txt"
-# echo "[1] ===========Storage Class Details===============" > "$FILENAME3"
-# kubectl get sc -A >> "$FILENAME3"
-# echo "==========================" >> "$FILENAME3"
-# kubectl describe sc -A >> "$FILENAME3"
-# echo "==========================" >> "$FILENAME3"
-# echo "[2] ===========PersistentVolume Details===============" >> "$FILENAME3"
-# kubectl get pv -A >> "$FILENAME3"
-# echo "==========================" >> "$FILENAME3"
-# kubectl describe pv -A >> "$FILENAME3"
-# echo "==========================" >> "$FILENAME3"
-# echo "[3] ===========PersistentVolume Claim Details===============" >> "$FILENAME3"
-# kubectl get pvc -A >> "$FILENAME3"
-# echo "==========================" >> "$FILENAME3"
-# kubectl describe pvc -A >> "$FILENAME3"
-# echo "==========================" >> "$FILENAME3"
+echo "Collecting information about all the persistance volume and storage class deployed ${CLUSTERNAME}, Review file: StorageInfo.txt  "
+FILENAME7="${OUTPUT_DIR}/StorageInfo.txt"
+echo "[1] ===========Storage Class Details===============" > "$FILENAME7"
+kubectl get sc -A >> "$FILENAME7"
+echo "==========================" >> "$FILENAME7"
+kubectl describe sc -A >> "$FILENAME7"
+echo "==========================" >> "$FILENAME7"
+echo "[2] ===========PersistentVolume Details===============" >> "$FILENAME7"
+kubectl get pv -A >> "$FILENAME7"
+echo "==========================" >> "$FILENAME7"
+kubectl describe pv -A >> "$FILENAME7"
+echo "==========================" >> "$FILENAME7"
+echo "[3] ===========PersistentVolume Claim Details===============" >> "$FILENAME7"
+kubectl get pvc -A >> "$FILENAME7"
+echo "==========================" >> "$FILENAME7"
+kubectl describe pvc -A >> "$FILENAME7"
+echo "==========================" >> "$FILENAME7"
+
+echo "Collecting information about WebHook configured in ${CLUSTERNAME}, Review file: WebHookInfo.txt  "
+FILENAME8="${OUTPUT_DIR}/WebHookInfo.txt"
+kubectl describe validatingwebhookconfigurations.admissionregistration.k8s.io -A > "$FILENAME8"
+kubectl describe mutatingwebhookconfigurations.admissionregistration.k8s.io -A >> "$FILENAME8"
+
+
 
 CWD=$(pwd)
 cd $ROOT_OUTPUT_DIR || exit 1
 
 echo " ***** INITIALIZING TARBALLING  ***** "
 echo "======= Collecting Errors and Failure logs, Review file: FoundErrors.txt ===================" 
-FILENAME5="${OUTPUT_DIR}/FoundErrors.txt"
-egrep -Ein "fail|err" "${OUTPUT_DIR}"/*.${EXTENSION} > "$FILENAME5"
+FILENAME9="${OUTPUT_DIR}/FoundErrors.txt"
+egrep -Ein "fail|err|off" "${OUTPUT_DIR}"/*.${EXTENSION} > "$FILENAME9"
+egrep -Ein "fail|err|off" "${OUTPUT_DIR}"/*.txt > "$FILENAME9"
 TARBALL_FILE_NAME="${OUTPUT_DIR_NAME}.tar.gz"
 echo "- File Created Successfully:  ${TARBALL_FILE_NAME} "
 tar -czf "./${TARBALL_FILE_NAME}" "./${OUTPUT_DIR_NAME}" 
@@ -118,4 +140,3 @@ echo " - Command to search log:  grep -Ei \"fail|err\" ${OUTPUT_DIR}/*.log"
 echo "========================== END OF SCRIPT EXECUTION ========================="
 
 cd "$CWD" || exit 1
-
