3c503cbb78b4464c2adddf3b04c92d31a7260cee
Milankumar Patel <pmh231294@gmail.com>
2023-02-13 20:00:53 -0800

    Included all the new PR suggestions and removed POD log collection
    I have removed the POD log collection due security/compliance reasons and included ONLY Deployment description and yaml file

--- eks-cluster-log-collector.sh
+++ eks-cluster-log-collector.sh
@@ -25,7 +25,8 @@ cd $ROOT_OUTPUT_DIR
 mkdir "$OUTPUT_DIR"
 
 echo "Collecting Information About Kubernetes Cluster: ${CLUSTERNAME}, Review File: ClusterDetails.txt "
-FILENAME="${OUTPUT_DIR}/ClusterDetails.txt"
+CLUSTER_INFO_FILE="${OUTPUT_DIR}/ClusterDetails.txt"
+FILENAME=CLUSTER_INFO_FILE
 append "$FILENAME"
 kubectl config current-context >> "$FILENAME"
 append "$FILENAME"
@@ -56,35 +57,38 @@ echo "===[2] =========== Kube-Proxy DaemonSet Details ===============" >> "$DAEM
 kubectl describe daemonset kube-proxy -n kube-system >> "$DAEMONSET"
 end_append "2" "$DAEMONSET"
 
-# Collecting All the PODs descriptions/logs running in User Desired Namespace Or by default it will collect pods log running in default namespace
+echo "Collecting CoreDNS Deployment Details From Kubernetes Cluster: ${CLUSTERNAME}, Review File: CoreDNS_Info.txt "
+COREDNS="${OUTPUT_DIR}/CoreDNS_Info.txt"
+echo "===[1] =========== CoreDNS Deployment Details ===============" > "$COREDNS"
+kubectl describe deployment coredns -n kube-system >> "$COREDNS"
+end_append "1" "$COREDNS"
+
+# Collecting All the deployment descriptions/ yaml file  deployed in User Desired Namespace Or by default it will collect pods log running in default namespace
 Default_Namespace=${1:-'default'}
-echo "Collecting All The Running POD Logs and POD Description From  Namespace:  ${Default_Namespace} "
+echo "Collecting All The Running Deployment Information In Namespace:  ${Default_Namespace}, Review File: Describe.txt, yaml.txt "
 kubectl get ns --no-headers | while read -r line; do
 NAMESPACE=$(echo "$line" | awk '{print $1}')
 if [[ "$NAMESPACE" = "$Default_Namespace" ]];then
-    kubectl get pods -n "$NAMESPACE" --no-headers | while read -r lines; do
-            POD_NAME=$(echo "$lines" | awk '{print $1}')
-            FILENAME1="${OUTPUT_DIR}/${NAMESPACE}.${POD_NAME}.describe.txt"
-            kubectl describe pod -n "$NAMESPACE" "$POD_NAME" > "$FILENAME1"
-            for CONTAINER in $(kubectl get po -n "$NAMESPACE" "$POD_NAME" -o jsonpath="{.spec.containers[*].name}"); do
-            FILENAME_PREFIX="${OUTPUT_DIR}/${NAMESPACE}.${POD_NAME}.${CONTAINER}"
-            
-            echo "Collecting Pod "{$POD_NAME}" Logs From "{$NAMESPACE}" Namespace "
-            FILENAME2="${FILENAME_PREFIX}.current.${EXTENSION}"
-            kubectl logs -n "$NAMESPACE" "$POD_NAME" --all-containers=true >"$FILENAME2"   
-        done 
-    done
+    kubectl get deployment -n "$NAMESPACE" --no-headers | while read -r lines; do
+            DEPLOYMENT_NAME=$(echo "$lines" | awk '{print $1}')
+            DEPLOYMENT="${OUTPUT_DIR}/${NAMESPACE}.${DEPLOYMENT_NAME}.describe.txt"
+            kubectl describe deployment -n "$NAMESPACE" "$DEPLOYMENT_NAME" > "$DEPLOYMENT"
+
+            DEPLOYMENT_YAML="${OUTPUT_DIR}/${NAMESPACE}.${DEPLOYMENT_NAME}.yaml.txt"
+            kubectl get deployment -n "$NAMESPACE" "$DEPLOYMENT_NAME" -o yaml > "$DEPLOYMENT_YAML"
+            done
 
 # Collecting All the K8 resources deployed in user specified namespace 
+
         echo "Collecting Information About All Other Deployed Resources in "{$NAMESPACE}" Namespace , Review File: AllResourcesInfo.txt"
-            FILENAME3="${OUTPUT_DIR}/AllResourcesInfo.txt"
-            append "$FILENAME3" 
-            kubectl get all -n "$NAMESPACE" >>"$FILENAME3" 
-            append "$FILENAME3" 
+            NS_RESOURCE_INFO="${OUTPUT_DIR}/AllResourcesInfo.txt"
+            append "$NS_RESOURCE_INFO" 
+            kubectl get all -n "$NAMESPACE" >>"$NS_RESOURCE_INFO" 
+            append "$NS_RESOURCE_INFO" 
 
         echo "Collecting Recent Events Log That Took Place Within Namespace ${NAMESPACE}, Review File: EventsInfo.txt"
-            FILENAME4="${OUTPUT_DIR}/EventsInfo.txt"
-            kubectl get events --sort-by=.metadata.creationTimestamp -n "$NAMESPACE" > "$FILENAME4" 
+            EVENT_INFO_FILE="${OUTPUT_DIR}/EventsInfo.txt"
+            kubectl get events --sort-by=.metadata.creationTimestamp -n "$NAMESPACE" > "$EVENT_INFO_FILE" 
  fi  
 done
 
@@ -92,43 +96,42 @@ echo "******* NOTE *******"
 print "Please Enter "yes" Or "y" If Your Current Issue Involves Kubernetes Resource Such As "{PVC, SC, PV, WorkerNode, WebHook}" So Script Can Continue Collect These Resource Information To Troubleshoot otherwise Enter "no" or "n""
 echo "*********************"
 read user_input
-input='yes'
-input2='y'
-if [ "$user_input" = "$input" ] || [ "$user_input" = "$input2" ];then
+
+if [ "$user_input" = "yes" ] || [ "$user_input" = "y" ];then
 
     echo "Collecting Information About All The Presently Running Worker Node In ${CLUSTERNAME}, Review File: WorkerNodeInfo.txt "
-    FILENAME6="${OUTPUT_DIR}/WorkerNodeInfo.txt"
-    kubectl describe node -A > "$FILENAME6"
+    NODE_INFO_FILE="${OUTPUT_DIR}/WorkerNodeInfo.txt"
+    kubectl describe node -A > "$NODE_INFO_FILE"
 
     # Collecting All the information about Persistent volume and storage class.
 
     echo "Collecting Information About All The Persistent Volume & Storage Class Deployed In ${CLUSTERNAME}, Review File: StorageInfo.txt  "
-    FILENAME7="${OUTPUT_DIR}/StorageInfo.txt"
-    echo "=== [1] ===========Storage Class Details===============" > "$FILENAME7"
-    kubectl get sc -A >> "$FILENAME7"
-    append "$FILENAME7"
-    kubectl describe sc -A >> "$FILENAME7"
-    end_append "1" "$FILENAME7"
+    STORAGE_INFO_FILE="${OUTPUT_DIR}/StorageInfo.txt"
+    echo "=== [1] ===========Storage Class Details===============" > "$STORAGE_INFO_FILE"
+    kubectl get sc -A >> "$STORAGE_INFO_FILE"
+    append "$STORAGE_INFO_FILE"
+    kubectl describe sc -A >> "$STORAGE_INFO_FILE"
+    end_append "1" "$STORAGE_INFO_FILE"
 
-    echo "=== [2] ===========PersistentVolume Details===============" >> "$FILENAME7"
-    kubectl get pv -A >> "$FILENAME7"
-    append "$FILENAME7"
-    kubectl describe pv -A >> "$FILENAME7"
-    end_append "2" "$FILENAME7"
+    echo "=== [2] ===========PersistentVolume Details===============" >> "$STORAGE_INFO_FILE"
+    kubectl get pv -A >> "$STORAGE_INFO_FILE"
+    append "$STORAGE_INFO_FILE"
+    kubectl describe pv -A >> "$STORAGE_INFO_FILE"
+    end_append "2" "$STORAGE_INFO_FILE"
    
-    echo "=== [3] ===========PersistentVolume Claim Details===============" >> "$FILENAME7"
-    kubectl get pvc -A >> "$FILENAME7"
-    append "$FILENAME7"
-    kubectl describe pvc -A >> "$FILENAME7"
-    end_append "3" "$FILENAME7"
+    echo "=== [3] ===========PersistentVolume Claim Details===============" >> "$STORAGE_INFO_FILE"
+    kubectl get pvc -A >> "$STORAGE_INFO_FILE"
+    append "$STORAGE_INFO_FILE"
+    kubectl describe pvc -A >> "$STORAGE_INFO_FILE"
+    end_append "3" "$STORAGE_INFO_FILE"
 
     echo "Collecting Information About All Configured WebHooks In ${CLUSTERNAME}, Review File: WebHookInfo.txt  "
-    FILENAME8="${OUTPUT_DIR}/WebHookInfo.txt"
-    append "$FILENAME8"
-    kubectl describe validatingwebhookconfigurations.admissionregistration.k8s.io -A >> "$FILENAME8"
-    append "$FILENAME8"
-    kubectl describe mutatingwebhookconfigurations.admissionregistration.k8s.io -A >> "$FILENAME8"
-    append "$FILENAME8"
+    WEBHOOK_INFO_FILE="${OUTPUT_DIR}/WebHookInfo.txt"
+    append "$WEBHOOK_INFO_FILE"
+    kubectl describe validatingwebhookconfigurations.admissionregistration.k8s.io -A >> "$WEBHOOK_INFO_FILE"
+    append "$WEBHOOK_INFO_FILE"
+    kubectl describe mutatingwebhookconfigurations.admissionregistration.k8s.io -A >> "$WEBHOOK_INFO_FILE"
+    append "$WEBHOOK_INFO_FILE"
 fi
 
 CWD=$(pwd)
@@ -137,9 +140,9 @@ cd $ROOT_OUTPUT_DIR || exit 1
 echo " ******* INITIALIZING TARBALLING  ********"
 
 print "======= Collecting All Recently Occurring Errors and Failure In ${CLUSTERNAME}  , Review File: FoundErrors.txt ===" 
-FILENAME9="${OUTPUT_DIR}/FoundErrors.txt"
-egrep -Ein "fail|err|off" "${OUTPUT_DIR}"/*.${EXTENSION} > "$FILENAME9"
-egrep -Ein "fail|err|off" "${OUTPUT_DIR}"/*.txt >> "$FILENAME9"
+FOUND_ERROR_FILE="${OUTPUT_DIR}/FoundErrors.txt"
+egrep -Ein "fail|err|off" "${OUTPUT_DIR}"/*.${EXTENSION} > "$FOUND_ERROR_FILE"
+egrep -Ein "fail|err|off" "${OUTPUT_DIR}"/*.txt >> "$FOUND_ERROR_FILE"
 TARBALL_FILE_NAME="${OUTPUT_DIR_NAME}.tar.gz"
 echo "- File Created Successfully:  ${TARBALL_FILE_NAME} "
 tar -czf "./${TARBALL_FILE_NAME}" "./${OUTPUT_DIR_NAME}" 
