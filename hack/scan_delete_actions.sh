3b9d424af2212b1e375ae3020f851d08cb214275
punkwalker <punkwalker@gmail.com>
2023-11-03 01:48:27 -0700

    Test destructive word

--- hack/scan_delete_actions.sh
+++ hack/scan_delete_actions.sh
@@ -2,7 +2,7 @@
 
 ls -altrh
 
-MATCH=$(grep -i 'delete\|remove' ../eks-pod-information-collector.sh)
+MATCH=$(grep -i 'delete\|remove' eks-pod-information-collector.sh)
 if [[ -n $MATCH ]]; then
     exit 1
 else
