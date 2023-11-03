27adeafa12a40e587a39b4f5d5dd26c401ee2a2a
punkwalker <punkwalker@gmail.com>
2023-11-03 01:37:39 -0700

    Add scan script

--- hack/scan_delete_actions.sh
+++ hack/scan_delete_actions.sh
@@ -2,9 +2,9 @@
 
 set -o errexit
 
-grep -i 'delete\|remove' eks-pod-information-collector.sh
-# if [[ $? -eq 0 ]]; then
-#     exit 1
-# else
-#     exit 0
-# fi
+grep -qi 'delete\|remove' eks-pod-information-collector.sh
+if [[ $? -eq 0 ]]; then
+    exit 1
+else
+    exit 0
+fi
