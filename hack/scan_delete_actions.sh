fdb8a5c8f03b4414b45cdbd263af0fe920bfa6f0
punkwalker <punkwalker@gmail.com>
2023-11-03 01:39:04 -0700

    Add scan script

--- hack/scan_delete_actions.sh
+++ hack/scan_delete_actions.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -o errexit
-
-grep -qi 'delete\|remove' eks-pod-information-collector.sh
+grep -qi 'delete\|remove' ../eks-pod-information-collector.sh
 if [[ $? -eq 0 ]]; then
     exit 1
 else
